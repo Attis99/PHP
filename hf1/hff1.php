7e299b2328e5cf4a7ee7971cf3a6831df88a3fb2
Attis99 <118990048+Attis99@users.noreply.github.com>
2022-11-24 16:55:12 +0200

    Add files via upload

---
+++ hf1/hff1.php
@@ -0,0 +1,70 @@
+<?php
+/*
+$nap = date("l");
+$datum = date("Y/m/d");
+echo "A mai datum: $datum";
+echo "<br>";
+if ($nap == "Monday") {
+   echo "A mai nap hetfo";
+ } elseif ($nap == "Tuesday") {
+    echo "A mai nap kedd";
+   } elseif ($nap == "Wednesday") {
+      echo "A mai nap szerda";
+  } elseif ($nap == "Thursday") {
+      echo "A mai nap csutortok";
+  } elseif ($nap == "Friday") {
+      echo "A mai nap pentek";
+  } elseif ($nap == "Saturday") {
+      echo "A mai nap szombat";
+  } elseif ($nap == "Sunday") {
+     echo "A mai nap vasarnap";
+}*/
+//2
+// $elso_szam = $_POST['elso_szam'] ?? null;
+// $masodik_szam = $_POST['masodik_szam'] ?? null;
+// $muvelet = $_POST['muvelet'] ?? null;
+// $eredmeny = '';
+//     if (is_numeric($elso_szam) && is_numeric($masodik_szam)) {
+//         switch ($muvelet) {
+//             case  "Hozzaad":
+//                 $eredmeny = $elso_szam + $masodik_szam;
+//                 break;
+//             case "Kivon":
+//                 $eredmeny = $elso_szam - $masodik_szam;
+//                 break;
+//             case "Szoroz":
+//                 $eredmeny = $elso_szam * $masodik_szam;
+//                 break;
+//             case "Eloszt":
+//                 $eredmeny = $elso_szam / $masodik_szam;
+//         }
+// }
+
+
+//3
+//echo "<table border='2', bgcolor='rgba(180,40,110,0.2)'><br />";
+//
+//for ($sor = 1; $sor <= 10; $sor ++) {
+//    echo "<tr>";
+//
+//    for ($oszlop = 1; $oszlop <= 10; $oszlop ++) {
+//        echo "<td> $oszlop / $sor<br>", number_format((float)($oszlop / $sor), 2, '.', ''), "</td>";
+//    }
+//
+//    echo "</tr>";
+//}
+//
+//echo "</table>";
+for ($x = 0; $x <9; $x++) {
+    echo "";
+}
+?>
+
+
+
+
+
+
+?>
+
+
